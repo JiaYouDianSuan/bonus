e8647cf5f054a740b068e7d0787adeffdcb059ee
magic <unmagic@yeah.com>
2016-08-26 15:00:34 +0800

    test

--- info.php
+++ info.php
@@ -4,6 +4,7 @@ require_once('Init.php');
 $sSql = "select * from bus_finance_payment";
 $oResult = Database::create()->query($sSql);
 
+echo "232323";
 while(list($iPk,$sNo) = mysql_fetch_array($oResult))
 
 {
